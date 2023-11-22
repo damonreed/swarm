36ff017b518579fc40a609ebaa8c331b4cc90fe4
Damon Reed <dreed@nomadia.org>
2023-11-22 16:01:25 -0600

    Initial POC and Docker testing

---
+++ terraform/main.tf
@@ -0,0 +1,65 @@
+#
+# swarm_mon - main.tf 
+#
+
+provider "google" {
+  project = var.project_id
+  region = var.region
+  zone = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-custom-mode-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "my-custom-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  network       = google_compute_network.vpc_network.id
+}
+
+# Create a single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "flask-vm"
+  machine_type = "e2-micro"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {image = "debian-cloud/debian-11"}
+  }
+
+  # Install Flask
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+    access_config {}
+  }
+}
+
+resource "google_compute_firewall" "flask" {
+  name    = "flask-app-firewall"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5000"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc_network.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+#
+# swarm_mon - variables.tf 
+#
+variable "project_id" { default = "swarm-poc-405612"}
+variable "region"     { default = "us-central1"     }
+variable "zone"       { default = "us-central1-a"   }
