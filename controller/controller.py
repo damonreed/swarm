7d9cbe73b097adeb5a5b5871b00e8ad284f352d9
Damon Reed <dreed@nomadia.org>
2023-11-30 07:01:00 -0600

    Fix google.auth call again

--- controller/controller.py
+++ controller/controller.py
@@ -3,10 +3,11 @@
 ###
 
 import google.auth
+from google.auth.transport.requests import AuthorizedSession
 
 credentials, project = google.auth.default()
 
-authed_session = google.auth.transport.requests.AuthorizedSession(credentials)
+authed_session = AuthorizedSession(credentials)
 
 response = authed_session.get('https://test-container-tnhs7yvhla-uc.a.run.app')
 
