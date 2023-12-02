e0e83b01a12509ad0335a1c6f6f9cd5764275b78
Damon Reed <dreed@nomadia.org>
2023-12-02 08:45:47 -0600

    Cleanup and refinement

--- controller/controller.py
+++ controller/controller.py
@@ -4,7 +4,6 @@
 
 from google.oauth2 import id_token
 from google.auth import compute_engine
-# from google.oauth2 import service_account
 import google.auth
 import google.auth.transport.requests
 from google.auth.transport.requests import AuthorizedSession
@@ -12,11 +11,8 @@ from google.auth.transport.requests import AuthorizedSession
 # target_audience = 'https://your-cloud-run-app.a.run.app'
 url = 'https://test-container-tnhs7yvhla-uc.a.run.app'
 
-#creds = service_account.IDTokenCredentials.from_service_account_file(
-#        '/path/to/svc.json', target_audience=target_audience)
 request = google.auth.transport.requests.Request()
 creds = compute_engine.IDTokenCredentials(request,target_audience=url)
-
 authed_session = AuthorizedSession(creds)
 
 # make authenticated request and print the response, status_code
--- test_function/main.py
+++ test_function/main.py
@@ -1,24 +1,12 @@
 import functions_framework
+from json import dumps
 
 @functions_framework.http
 def test(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+    args = dumps(dict(request.args))
+    host = request.remote_addr
+    json = request.get_json(silent=True)
+    
+    results = f"args = {args}\nhost = {host}\njson = {json}\n"
 
-    if request_json and 'host' in request_json:
-        host = request_json['host']
-    elif request_args and 'host' in request_args:
-        host = request_args['host']
-    else:
-        host = request.remote_addr
-
-    return f'Hello {host}!\n'
+    return results
